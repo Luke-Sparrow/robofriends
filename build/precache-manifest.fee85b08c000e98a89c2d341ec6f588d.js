94a4d5d0c3478e91d06e563a9bf3bcaaed5e3671
Luke Sparrow <luke.sparrow@pm.me>
2020-07-29 19:22:46 -0600

    updated navbar and added scrolling

---
+++ build/precache-manifest.fee85b08c000e98a89c2d341ec6f588d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a70ae868aac8e7a4b820fb29c14923b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dae49e058e1199e3f891",
+    "url": "/static/css/main.fea07362.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae49e058e1199e3f891",
+    "url": "/static/js/main.f75bcec1.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "a4a76573543e193e178862351e5dd232",
+    "url": "/static/media/blade_runner.a4a76573.woff"
+  }
+]);
